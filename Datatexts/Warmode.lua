1dceff8d026c8b29f215e69ce1725d3f341e42ed
Caedis <1590-Caedis@users.noreply.git.tukui.org>
2020-10-13 20:39:56 -0500

    Redone the War Mode DT

--- Datatexts/Warmode.lua
+++ Datatexts/Warmode.lua
@@ -11,6 +11,13 @@ local C_PvP_IsWarModeDesired = C_PvP.IsWarModeDesired
 local C_PvP_IsWarModeFeatureEnabled = C_PvP.IsWarModeFeatureEnabled
 local C_PvP_ToggleWarMode = C_PvP.ToggleWarMode
 local C_PvP_CanToggleWarMode = C_PvP.CanToggleWarMode
+local C_PvP_CanToggleWarModeInArea = C_PvP.CanToggleWarModeInArea
+local C_PvP_GetWarModeRewardBonus = C_PvP.GetWarModeRewardBonus
+local IsResting = IsResting
+local UnitFactionGroup = UnitFactionGroup
+
+local GameTooltip_AddColoredLine = GameTooltip_AddColoredLine
+local GameTooltip_AddNormalLine = GameTooltip_AddNormalLine
 
 local InCombatLockdown = InCombatLockdown
 
@@ -21,7 +28,30 @@ local function OnEnter(self)
 
     DT.tooltip:ClearLines()
 
-    DT.tooltip:AddLine("|cffFFFFFFLeft Click:|r Toggle War Mode")
+    local warModeRewardBonus = C_PvP_GetWarModeRewardBonus()
+
+    GameTooltip_AddNormalLine(DT.tooltip, PVP_WAR_MODE_DESCRIPTION_FORMAT:format(warModeRewardBonus), true)
+
+    local canToggleWarmode = C_PvP_CanToggleWarMode(true);
+    local canToggleWarmodeOFF = C_PvP_CanToggleWarMode(false);
+    
+    if (not canToggleWarmode or not canToggleWarmodeOFF) then
+
+        if(not C_PvP_CanToggleWarModeInArea()) then
+            if(C_PvP_IsWarModeDesired()) then
+                if(not canToggleWarmodeOFF and not IsResting()) then
+                    warmodeErrorText = UnitFactionGroup("player") == PLAYER_FACTION_GROUP[0] and PVP_WAR_MODE_NOT_NOW_HORDE_RESTAREA or PVP_WAR_MODE_NOT_NOW_ALLIANCE_RESTAREA;
+                end
+            else
+                if(not canToggleWarmode) then
+                    warmodeErrorText = UnitFactionGroup("player") == PLAYER_FACTION_GROUP[0] and PVP_WAR_MODE_NOT_NOW_HORDE or PVP_WAR_MODE_NOT_NOW_ALLIANCE;
+                end
+            end
+        end
+
+        GameTooltip_AddColoredLine(DT.tooltip, warmodeErrorText, RED_FONT_COLOR, true)
+    else
+    end
 
     DT.tooltip:Show()
 end
@@ -31,7 +61,7 @@ function OnClick(self, button)
     if button == "LeftButton" then
         DT.tooltip:Hide()
 
-        if C_PvP_CanToggleWarMode() then
+        if C_PvP_CanToggleWarMode(not C_PvP_IsWarModeActive()) then
             C_PvP_ToggleWarMode()
         end
     end
@@ -66,4 +96,4 @@ local events = {
 	click - function to fire when clicking the datatext
 	onEnterFunc - function to fire OnEnter
 ]]
-DT:RegisterDatatext("War Mode", events, OnEvent, nil, OnClick, OnEnter)
+DT:RegisterDatatext("War Mode", 'ExtraDataTexts', events, OnEvent, nil, OnClick, OnEnter)
